1c29bbcf792ec03863ee59409bc916bd80229e4a
Chad Cunningham <chadcunningham7676@gmail.com>
2021-06-06 18:36:43 -0400

    Updated table columns

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,12 @@ ActiveRecord::Schema.define(version: 2021_06_06_222617) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.string "password", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
 end
