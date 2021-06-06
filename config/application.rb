e6ab6cb36ad09a1474c13c761eb39ea0465de422
Chad Cunningham <chadcunningham7676@gmail.com>
2021-06-06 18:26:01 -0400

    Added rspec, guard, and spring

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,15 @@ module GoalsApp
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => false,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => false
+    end
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
