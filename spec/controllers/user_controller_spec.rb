988dfe9e1addc3d6330579a9129c04a3f3d9d2fe
Chad Cunningham <chadcunningham7676@gmail.com>
2021-06-06 20:13:01 -0400

    Completed initial tests for Users controller

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe UsersController, type: :controller do
 
   describe "GET #new" do
     it "renders user new template" do
-      get :new, user: {}
-      expect(respone).to render_template('new')
+      get :new
+      expect(response).to render_template('new')
       expect(response).to have_http_status(200)
     end
   end
@@ -21,7 +21,7 @@ RSpec.describe UsersController, type: :controller do
 
     context 'with valid params' do 
       it "redirects to user show page" do
-        get :create, params: { user: { email: 'test@test.com', password: 'passsword' } }
+        get :create, params: { user: { email: 'test@test.com', password: 'password' } }
         expect(response).to redirect_to(user_url(User.last))
       end
     end
@@ -29,7 +29,8 @@ RSpec.describe UsersController, type: :controller do
 
   describe "GET #show" do
     it "renders user show template" do
-      get :new
+      user = User.create!(email: 'test2@test.com', password: 'password')
+      get :show, params: { id: user.id }
       expect(response).to render_template('show')
     end
   end
