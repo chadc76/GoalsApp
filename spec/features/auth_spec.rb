8845fc10441f65f0813ef1a1002964faf759858b
Chad Cunningham <chadcunningham7676@gmail.com>
2021-06-11 16:29:45 -0400

    Updated tests

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -1,55 +1,57 @@
 require 'spec_helper'
 require 'rails_helper'
 
-feature 'the signup process' do
-  scenario 'has a new user page' do
-    visit new_user_url
-    expect(page).to have_content("Sign up!")
-  end
-
-  feature 'signing up a user' do
-
-    scenario 'shows email on the homepage after signup' do
+feature "Authentication features", type: :feature do
+  feature 'the signup process' do
+    scenario 'has a new user page' do
       visit new_user_url
+      expect(page).to have_content("Sign up!")
+    end
+  
+    feature 'signing up a user' do
+  
+      scenario 'shows email on the homepage after signup' do
+        visit new_user_url
+        fill_in('Email', with: 'test@test.com')
+        fill_in('Password', with: 'password')
+        click_button("Sign up!")
+        expect(page).to have_content('test@test.com')
+      end
+  
+    end
+  end
+  
+  feature 'logging in' do
+  
+    scenario 'shows username on the homepage after login' do
+      User.create!(email: 'test@test.com', password: 'password')
+      visit new_session_url
       fill_in('Email', with: 'test@test.com')
       fill_in('Password', with: 'password')
-      click_button("Sign up!")
+      click_button("Log in!")
       expect(page).to have_content('test@test.com')
     end
-
+  
   end
-end
-
-feature 'logging in' do
-
-  scenario 'shows username on the homepage after login' do
-    User.create!(email: 'test@test.com', password: 'password')
-    visit new_session_url
-    fill_in('Email', with: 'test@test.com')
-    fill_in('Password', with: 'password')
-    click_button("Log in!")
-    expect(page).to have_content('test@test.com')
+  
+  feature 'logging out' do
+    before(:each) do
+      User.create!(email: 'test@test.com', password: 'password')
+      visit new_session_url
+      fill_in('Email', with: 'test@test.com')
+      fill_in('Password', with: 'password')
+      click_button("Log in!")
+    end
+  
+    scenario 'begins with a logged out state' do
+      click_button("Log out")
+      expect(page).to have_content("Log in!")
+    end
+  
+    scenario 'doesn\'t show username on the homepage after logout' do
+      click_button("Log out")
+      expect(page).not_to have_content("test@test.com")
+    end
+  
   end
-
-end
-
-feature 'logging out' do
-  before(:each) do
-    User.create!(email: 'test@test.com', password: 'password')
-    visit new_session_url
-    fill_in('Email', with: 'test@test.com')
-    fill_in('Password', with: 'password')
-    click_button("Log in!")
-  end
-
-  scenario 'begins with a logged out state' do
-    click_button("Log out")
-    expect(page).to have_content("Log in!")
-  end
-
-  scenario 'doesn\'t show username on the homepage after logout' do
-    click_button("Log out")
-    expect(page).not_to have_content("test@test.com")
-  end
-
 end
---
+++ spec/features/goal_features_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature "Goal features", type: :feature do
+  feature  "adding a new goal" do
+    before(:each) { log_in }
+  
+    scenario "has new goal page" do
+      visit new_goal_url
+      expect(page).to have_content("New Goal")
+    end
+    
+    scenario "with invalid params" do
+      create_goal("")
+      expect(current_path).to eq("/goals")
+      expect(page).to have_content("Title can't be blank")
+    end
+  
+    scenario "with valid params" do
+      create_goal("Test Title", "Test details")
+      expect(page).to have_content("Test Title")
+      expect(current_path).to eq("/goals/#{Goal.last.id}")
+    end
+  end
+  
+  feature  "editing a goal" do
+    before(:each) do
+      log_in
+      create_goal("Test Title", "Test details")
+    end
+  
+    scenario "has edit goal page" do
+      visit edit_goal_url(Goal.last)
+      expect(page).to have_content("Edit Goal")
+    end
+    
+    scenario "with invalid params" do
+      edit_goal("")
+      expect(current_path).to eq("/goals/#{Goal.last.id}")
+      expect(page).to have_content("Title can't be blank")
+    end
+  
+    scenario "with valid params" do
+      edit_goal("New Test Title", "new Test details", true, true)
+      expect(page).to have_content("New Test Title")
+      expect(current_path).to eq("/goals/#{Goal.last.id}")
+    end
+  end
+
+  feature "deleting a goal" do
+    before(:each) do
+      log_in
+      create_goal("Test Title", "Test details")
+    end
+  
+    scenario "able to delete from user show page" do
+      visit user_url(User.last)
+      click_button("delete #{Goal.last.title} goal")
+    end
+  
+    scenario "able to delete from users goal index page" do
+      visit goals_url
+      click_button("delete #{Goal.last.title} goal")
+    end
+  end
+end
