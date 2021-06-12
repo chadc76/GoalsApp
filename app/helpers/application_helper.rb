24156018767f9d284596d4560ad47d2617d0fe96
Chad Cunningham <chadcunningham7676@gmail.com>
2021-06-12 16:50:24 -0400

    Completed completion feature tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def auth_token
     "<input type=\"hidden\" name=\"authenticity_token\" value=\"#{form_authenticity_token}\">".html_safe
   end
+
+  def completion_buttons(goal)
+    if goal.complete
+      button_to("Oops! Did not complete.", toggle_complete_goal_url(goal), method: :post)
+    else
+      button_to("Complete", toggle_complete_goal_url(goal), method: :post)
+    end
+  end
 end
