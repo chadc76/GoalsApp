e52edca6f2d2335a2220ed25a1f165edfedf182e
Chad Cunningham <chadcunningham7676@gmail.com>
2021-06-12 16:50:13 -0400

    Added tests for completion feature

--- spec/features/goal_features_spec.rb
+++ spec/features/goal_features_spec.rb
@@ -21,6 +21,12 @@ feature "Goal features", type: :feature do
       expect(page).to have_content("Test Title")
       expect(current_path).to eq("/goals/#{Goal.last.id}")
     end
+
+    scenario "able to add goal from user show page" do
+      visit user_url(User.last)
+      click_link("New Goal")
+      expect(current_path).to eq("/goals/new")
+    end
   end
   
   feature  "editing a goal" do
@@ -61,6 +67,65 @@ feature "Goal features", type: :feature do
     scenario "able to delete from users goal index page" do
       visit goals_url
       click_button("delete #{Goal.last.title} goal")
+    end
+  end
+
+  feature "completing a goal" do
+    before(:each) do 
+      log_in
+      create_goal("Test Title", "Test details")
+    end
+
+    scenario "able to complete goal from user show page" do
+      user = User.last
+      visit user_url(user)
+      click_button("Complete")
+      expect(current_url).to eq(user_url(user))
+      expect(page).to have_selector(:link_or_button, "Oops! Did not complete.")
+    end
+
+    scenario "able to complete goal from goal show page" do
+      goal = Goal.last
+      visit goal_url(goal)
+      click_button("Complete")
+      expect(current_url).to eq(goal_url(goal))
+      expect(page).to have_selector(:link_or_button, "Oops! Did not complete.")
+    end
+
+    scenario "able to complete goal from goals index page" do
+      visit goals_url
+      click_button("Complete")
+      expect(current_url).to eq(goals_url)
+      expect(page).to have_selector(:link_or_button, "Oops! Did not complete.")
+    end
+
+    scenario "able to uncomplete goal from user show page" do
+      goal = Goal.last
+      goal.toggle!(:complete)
+      user = User.last
+      visit user_url(user)
+      click_button("Oops! Did not complete.")
+      expect(current_url).to eq(user_url(user))
+      expect(page).to have_selector(:link_or_button,"Complete")
+    end
+
+    scenario "able to uncomplete goal from goal show page" do
+      goal = Goal.last
+      goal.toggle!(:complete)
+      visit goal_url(goal)
+      goal.save
+      click_button("Oops! Did not complete.")
+      expect(current_url).to eq(goal_url(goal))
+      expect(page).to have_selector(:link_or_button,"Complete")
+    end
+
+    scenario "able to uncomplete goal from goals index page" do
+      goal = Goal.last
+      goal.toggle!(:complete)
+      visit goals_url
+      click_button("Oops! Did not complete.")
+      expect(current_url).to eq(goals_url)
+      expect(page).to have_selector(:link_or_button,"Complete")
     end
   end
 end
